87492f39ece05dcc2c0ce1364c59fecb49d09118
Jefferson <jefferson.brandao@g.globo>
2023-12-17 23:52:29 -0300

    criando footer

--- components/shared/base/footer.tsx
+++ components/shared/base/footer.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+        </Link>
+
+        <p>2023 Evently. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
