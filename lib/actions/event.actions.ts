c996c15efeb29a8baab84e86600736c709ba6031
Jefferson <jefferson.brandao@g.globo>
2023-12-18 22:38:22 -0300

    criando pagina de evento

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,10 +1,18 @@
 "use server"
 
 import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
 import { CreateEventParams } from "../types";
 import { handleError } from "../utils";
+
+async function populateEvent(query: any) {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Category, select: '_id name' })
+
+}
 
 export async function createEvent({ event, userId, path }: CreateEventParams) {
   try {
@@ -19,6 +27,22 @@ export async function createEvent({ event, userId, path }: CreateEventParams) {
     const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
 
     return JSON.parse(JSON.stringify(newEvent))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase()
+
+    const event = await populateEvent(Event.findById(eventId))
+
+    if (!event) {
+      throw new Error("Event not found")
+    }
+
+    return JSON.parse(JSON.stringify(event))
   } catch (error) {
     handleError(error)
   }
